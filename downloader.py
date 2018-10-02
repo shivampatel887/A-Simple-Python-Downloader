febd85bb3a4abd926a990910cbecf4bc9731b511
shivampatel887 <shivampatel887@gmail.com>
2018-10-02 21:10:26 +0530

    tk update

--- downloader.py
+++ downloader.py
@@ -1,12 +1,32 @@
 import random
 import urllib.request
+from tkinter import *
 
-extent = input("What is your file type ?\n")
 
-url = input("Paste Your URL\n")
+
 def Downloader(url):
     name = random.randrange(1,10000)
     filename = str(name) + '.' + extent 
     urllib.request.urlretrieve(url,filename)
-        
-Downloader(url)
+
+base = Tk()
+base.title("Downloader")
+
+heading = Label(base, text="Enter Your File type")
+heading.pack()
+enterText = Entry(base,exportselection=0)
+enterText.pack()
+
+SubHeading = Label(base, text="Paste Your URL")
+SubHeading.pack()
+enterURL = Entry(base,exportselection=0)
+enterURL.pack()
+
+extent = enterText.get()
+url = str(enterURL.get())
+
+okay = Button(base ,command=lambda: Downloader(url) ,text="Download")
+okay.pack()
+
+
+base.mainloop()
